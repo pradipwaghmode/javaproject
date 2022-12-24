d41db7146e8748b1113eac2978343fb1fdf39f7a
pradipwaghmode <pradipwaghmode7@gmail.com>
2022-12-24 18:27:46 +0530

    first commit : old code

---
+++ src/com/pradip/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.pradip;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		
+		al.add("pradip");
+		al.add("shyam");
+		al.add("shivani");
+		
+		System.out.println(al);
+	}
+
+}
